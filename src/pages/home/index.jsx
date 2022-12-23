04861b7e1d39d83446898fe4f15eb6ba72285cc9
MATHEUS CARVALHO <matheuscarvalho.pe@hotmail.com>
2022-12-23 16:11:48 -0300

    adicionando botoes atraves do react(styled-components)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 
+import { Button } from "../../components/button";
+
 const Home = () => {
     return (<>
         <h1>HOME</h1>
+        <Button title="teste1" />
+        <Button variant={'secondary'} title="teste2" /> 
         <Link to="/login">Entra</Link>
     </>)
 }
